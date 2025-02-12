fec4c76c546789f7b66f56926d6c300458b760ac
Adityakuma123 <164056143+Adhikulkarn@users.noreply.github.com>
2025-02-12 15:48:27 +0530

    Update 1
    Added js functionality for contacts instead of <a>-----</a>

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("cv").addEventListener("click", function() {
+    alert("Resume will be uploaded Soon !"); 
+});
+document.getElementById("abtme").addEventListener("click", function() {
+    alert("Blogs Will Be Uploaded Soon"); 
+});
+document.getElementById("linkedin").addEventListener("click", function() {
+window.location.href = "https://www.linkedin.com/in/aditya-kulkarni-577344326";
+});
+document.getElementById("github").addEventListener("click", function() {
+window.location.href = "https://github.com/Adhikulkarn";
+});
+document.getElementById("mail").addEventListener("click", function() {
+window.location.href = "mailto:adityask200615@gmail.com";
+});
+document.getElementById("insta").addEventListener("click", function() {
+window.location.href = "https://www.instagram.com/adhizoooo_";
+});
